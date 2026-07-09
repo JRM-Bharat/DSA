d9bacd8fc2e1e035d4763401eba26bf4b28f070d
Jyotiranjan Moharana <jr.moharana2340@gmail.com>
2026-07-09 16:23:18 +0530

    Time: 85 ms (21.91%) | Memory: 93.7 MB (89.32%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        /* //Solution using Hash Maps
+        
         unordered_map<int,int>checked;
         for(int i=0; i<nums.size(); i++){
             if(checked.count(nums[i]) && i-checked[nums[i]]<=k){
                 return true;
             }
             checked[nums[i]]=i;
+        }
+        return false;*/
+
+        // Solution using Hash Sets and Sliding Window
+        
+        unordered_set<int>slidWin;
+        for(int i=0; i<nums.size(); i++){
+            if(i>k) slidWin.erase(nums[i-k-1]);
+            if(slidWin.count(nums[i])) return true;
+            slidWin.insert(nums[i]);
         }
         return false;
     }
